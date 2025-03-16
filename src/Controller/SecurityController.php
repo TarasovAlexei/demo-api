ce7876a105d3f4978c4dbce2097706fba208bf29
Alexei <alexeitara5ov@yandex.ru>
2025-03-16 18:36:18 +0300

    Add api login form with json_login

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use ApiPlatform\Metadata\IriConverterInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+final class SecurityController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function login(IriConverterInterface $iriConverter, #[CurrentUser] $user = null): Response
+    {
+        if (!$user) {
+            return $this->render('login/index.html.twig');
+        }
+
+        return new Response(null, 204, [
+            'Location' => $iriConverter->getIriFromResource($user),
+        ]);
+
+
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \Exception('This should never be reached!');
+    }
+
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,22 +2,37 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use App\Factory\PublicationFactory;
 use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
-{
+{   
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher,
+    ) {
+    }
+
     public function load(ObjectManager $manager): void
     {
+        UserFactory::createOne([
+            'email' => 'jane@symfony.com',
+            'password' => 'kitten',
+            'firstName' => 'Jane',
+            'lastName' => 'Die',
+        ]);
+        
         UserFactory::createMany(4);
+
         PublicationFactory::createMany(40, function () {
             return [
                 'author' => UserFactory::random(),
             ];
         });
 
-        $manager->flush();
+
     }
 }
