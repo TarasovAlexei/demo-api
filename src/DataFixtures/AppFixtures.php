bc23a56928e3eb256d477cfee57c876e738b62bc
Alexei <alexeitara5ov@yandex.ru>
2025-05-03 18:14:10 +0300

    Add access token authenticator

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\ApiTokenFactory;
 use App\Factory\BlogPostFactory;
 use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -11,10 +12,21 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {   
+        UserFactory::createOne([
+            'email' => 'jane@symfony.com',
+            'password' => 'kitten',
+        ]);
+
         UserFactory::createMany(10);
         BlogPostFactory::createMany(40, function () {
             return [
                 'author' => UserFactory::random(),
+            ];
+        });
+
+        ApiTokenFactory::createMany(30, function () {
+            return [
+                'ownedBy' => UserFactory::random(),
             ];
         });
     }
