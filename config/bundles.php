40df8616e9fefb9ae5b94d490bd1f43d223e9ee7
Alexei <alexeitara5ov@yandex.ru>
2025-05-28 20:07:05 +0300

    add: symfonycasts/tailwind-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Vue\VueBundle::class => ['all' => true],
+    Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
 ];
