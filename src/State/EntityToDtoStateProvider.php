491cb76871980ed6e46cfd78133f48a633bf1ee8
Alexei <alexeitara5ov@yandex.ru>
2025-06-13 19:59:53 +0300

    добавлено: возврат одного элемента ItemProvider

--- src/State/EntityToDtoStateProvider.php
+++ src/State/EntityToDtoStateProvider.php
@@ -2,39 +2,54 @@
 
 namespace App\State;
 
+use ApiPlatform\Doctrine\Orm\State\ItemProvider;
+use ApiPlatform\Metadata\CollectionOperationInterface;
 use ApiPlatform\Doctrine\Orm\Paginator;
-use ApiPlatform\Metadata\Operation;
-use ApiPlatform\State\ProviderInterface;
-use ApiPlatform\State\Pagination\TraversablePaginator;
 use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\Pagination\TraversablePaginator;
+use ApiPlatform\State\ProviderInterface;
 use App\ApiResource\UserApi;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 
 class EntityToDtoStateProvider implements ProviderInterface
 {   
     public function __construct(
-        #[Autowire(service: CollectionProvider::class)] private ProviderInterface $collectionProvider
+        #[Autowire(service: CollectionProvider::class)] private ProviderInterface $collectionProvider,
+        #[Autowire(service: ItemProvider::class)] private ProviderInterface $itemProvider,
     )
     {
     }
 
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
-        $entities = $this->collectionProvider->provide($operation, $uriVariables, $context);
-        assert($entities instanceof Paginator);
+        if ($operation instanceof CollectionOperationInterface) {
+            $entities = $this->collectionProvider->provide($operation, $uriVariables, $context);
 
-        $dtos = [];
-        foreach ($entities as $entity) {
-            $dtos[] = $this->mapEntityToDto($entity);
+            assert($entities instanceof Paginator);
+
+            $dtos = [];
+            foreach ($entities as $entity) {
+                $dtos[] = $this->mapEntityToDto($entity);
+            }
+
+            return new TraversablePaginator(
+                new \ArrayIterator($dtos),
+                $entities->getCurrentPage(),
+                $entities->getItemsPerPage(),
+                $entities->getTotalItems()
+            );
         }
-        
-        return new TraversablePaginator(
-            new \ArrayIterator($dtos),
-            $entities->getCurrentPage(),
-            $entities->getItemsPerPage(),
-            $entities->getTotalItems()
-        );
+
+        $entity = $this->itemProvider->provide($operation, $uriVariables, $context);
+
+        if (!$entity) {
+            return null;
+        }
+
+        return $this->mapEntityToDto($entity);
+
     }
 
     private function mapEntityToDto(object $entity): object
