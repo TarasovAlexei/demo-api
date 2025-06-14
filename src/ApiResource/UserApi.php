0facdd14b130fbe6bac0a06db1f530f95e012c78
Alexei <alexeitara5ov@yandex.ru>
2025-06-14 19:43:22 +0300

    добавлено: валидация

--- src/ApiResource/UserApi.php
+++ src/ApiResource/UserApi.php
@@ -5,13 +5,28 @@ namespace App\ApiResource;
 use ApiPlatform\Doctrine\Orm\State\Options;
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
 use App\Entity\BlogPost;
 use App\Entity\User;
 use App\State\EntityToDtoStateProvider;
 use App\State\EntityClassDtoStateProcessor;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ApiResource(
     shortName: 'User',
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(
+            validationContext: ['groups' => ['Default', 'postValidation']],
+        ),
+        new Patch(),
+        new Delete(),
+    ],
     paginationItemsPerPage: 5,
     provider: EntityToDtoStateProvider::class,
     processor: EntityClassDtoStateProcessor::class,
@@ -22,16 +37,21 @@ class UserApi
     #[ApiProperty(readable: false, writable: false, identifier: true)]
     public ?int $id = null;
 
+    #[Assert\NotBlank]
+    #[Assert\Email]
     public ?string $email = null;
 
+    #[Assert\NotBlank]
     public ?string $firstName = null;
 
+    #[Assert\NotBlank]
     public ?string $lastName = null;
 
     /**
      * The plaintext password when being set or changed.
      */
     #[ApiProperty(readable: false)]
+    #[Assert\NotBlank(groups: ['postValidation'])]
     public ?string $password = null;
 
     /**
