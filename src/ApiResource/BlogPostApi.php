45da9398b711b649658ceae73dfc4391a653a3ae
Alexei <alexeitara5ov@yandex.ru>
2025-06-16 19:43:54 +0300

    добавить: опции security к операциям с конечными точками

--- src/ApiResource/BlogPostApi.php
+++ src/ApiResource/BlogPostApi.php
@@ -5,6 +5,11 @@ namespace App\ApiResource;
 use ApiPlatform\Doctrine\Orm\State\Options;
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
 use App\Entity\BlogPost;
 use App\State\EntityClassDtoStateProcessor;
 use App\State\EntityToDtoStateProvider;
@@ -13,6 +18,17 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 
 #[ApiResource(
     shortName: 'Post',
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(
+            security: 'is_granted("ROLE_POST_CREATE")',
+        ),
+        new Patch(
+            security: 'is_granted("EDIT", object)',
+        ),
+        new Delete()
+    ],
     paginationItemsPerPage: 10,
     provider: EntityToDtoStateProvider::class,
     processor: EntityClassDtoStateProcessor::class,
