db1a0008737411a5953ab35a7d4e57ce503d3fd2
Alexei <alexeitara5ov@yandex.ru>
2025-08-14 20:23:42 +0300

    Тестировать аутентификацию на основе токенов

--- tests/Functional/BlogPostResourceTest.php
+++ tests/Functional/BlogPostResourceTest.php
@@ -2,16 +2,20 @@
 
 namespace App\Tests\Functional;
 
+use App\Entity\ApiToken;
+use App\Factory\ApiTokenFactory;
 use App\Factory\BlogPostFactory;
 use App\Factory\UserFactory;
 use Zenstruck\Browser\Test\HasBrowser;
 use Zenstruck\Foundry\Test\ResetDatabase;
 use Zenstruck\Browser\HttpOptions;
 use Zenstruck\Browser\Json;
+use Zenstruck\Foundry\Test\Factories;
 
 class BlogPostResourceTest extends ApiTestCase
 {
     use ResetDatabase;
+    use Factories;
 
     public function testToCreatePost(): void
     {
@@ -30,6 +34,40 @@ class BlogPostResourceTest extends ApiTestCase
             ]))
             ->assertStatus(201)
             ->assertJsonMatches('title', 'Title')
+        ;
+    }
+
+    public function testToCreatePostWithApiKey(): void
+    {
+        $token = ApiTokenFactory::createOne([
+            'scopes' => [ApiToken::SCOPE_POST_CREATE]
+        ]);
+
+        $this->browser()
+            ->post('/api/posts', [
+                'json' => [],
+                'headers' => [
+                    'Authorization' => 'Bearer '.$token->getToken()
+                ]
+            ])
+            ->assertStatus(422)
+        ;
+    }
+
+     public function testToCreatePostDeniedWithoutScope(): void
+    {
+        $token = ApiTokenFactory::createOne([
+            'scopes' => [ApiToken::SCOPE_POST_EDIT]
+        ]);
+
+        $this->browser()
+            ->post('/api/posts', [
+                'json' => [],
+                'headers' => [
+                    'Authorization' => 'Bearer '.$token->getToken()
+                ]
+            ])
+            ->assertStatus(403)
         ;
     }
 }
