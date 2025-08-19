15211c3df3f702751ae774ea3f3ac74d7a5ebf28
Alexei <alexeitara5ov@yandex.ru>
2025-08-19 19:53:55 +0300

    Декорировать поставщик состояний для конечной точки коллекции

--- src/State/BlogPostStateProvider.php
+++ src/State/BlogPostStateProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\State;
 
+use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
+use ApiPlatform\Metadata\CollectionOperationInterface;
 use ApiPlatform\Doctrine\Orm\State\ItemProvider;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
@@ -13,13 +15,25 @@ class BlogPostStateProvider implements ProviderInterface
 {   
     public function __construct(
         #[Autowire(service: ItemProvider::class)] private ProviderInterface $itemProvider,
+        #[Autowire(service: CollectionProvider::class)] private ProviderInterface $collectionProvider,
         private Security $security,
     )
     {
     }
 
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
-    {
+    {   
+        if ($operation instanceof CollectionOperationInterface) {
+            /** @var $paginator iterable<BlogPost> */
+            $paginator = $this->collectionProvider->provide($operation, $uriVariables, $context);
+
+            foreach ($paginator as $post) {
+                $post->setIsAuthorByAuthenticatedUser($this->security->getUser() === $post->getAuthor());
+            }
+
+            return $paginator;
+        }
+
         $post = $this->itemProvider->provide($operation, $uriVariables, $context);
 
         if (!$post instanceof BlogPost) {
