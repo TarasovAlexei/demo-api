cd0a15f0b7d5216c4c7dc7a29dff679102e12879
Alexey <alexeitara5ov@yandex.ru>
2025-12-30 18:28:44 +0300

    Добавить виртуальное поле

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -9,6 +9,7 @@ use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Post;
 use ApiPlatform\Metadata\Delete;
 use App\Repository\BlogPostRepository;
+use Carbon\Carbon;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
@@ -89,6 +90,12 @@ class BlogPost
     public function getCreatedAt(): ?\DateTimeImmutable
     {
         return $this->createdAt;
+    }
+
+    #[Groups(['post:read'])]
+    public function getCreatedAtAgo(): string
+    {
+        return Carbon::instance($this->createdAt)->diffForHumans();
     }
 
     public function getIsPublished(): ?bool
