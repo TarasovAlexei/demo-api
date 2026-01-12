149d003cfcc5e0fc5b7e20dbac47796b6d5ebcc9
Alexey <alexeitara5ov@yandex.ru>
2026-01-12 17:24:57 +0300

    Настроить безопасность и валидацию для DTO

--- src/ApiResource/UserApi.php
+++ src/ApiResource/UserApi.php
@@ -6,15 +6,34 @@ use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Doctrine\Orm\State\Options;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
 use ApiPlatform\Metadata\ApiProperty;
 use App\Entity\BlogPost;
 use App\State\EntityClassDtoStateProcessor;
 use App\State\EntityToDtoStateProvider;
 use App\Entity\User;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ApiResource(
     shortName: 'User',
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(
+            validationContext: ['groups' => ['Default', 'postValidation']],
+            security: 'is_granted("PUBLIC_ACCESS")',
+        ),
+        new Patch(
+            security: 'is_granted("ROLE_USER_EDIT")'
+        ),
+        new Delete(),
+    ],
     paginationItemsPerPage: 5,
+    security: 'is_granted("ROLE_USER")',
     provider: EntityToDtoStateProvider::class,
     processor: EntityClassDtoStateProcessor::class,
     stateOptions: new Options(entityClass: User::class),
@@ -27,13 +46,18 @@ class UserApi
     #[ApiProperty(readable: false, writable: false, identifier: true)]
     public ?int $id = null;
 
+    #[Assert\NotBlank]
+    #[Assert\Email]
     public ?string $email = null;
 
+    #[Assert\NotBlank]
     public ?string $firstName = null;
 
+    #[Assert\NotBlank]
     public ?string $lastName = null;
 
     #[ApiProperty(readable: false)]
+    #[Assert\NotBlank(groups: ['postValidation'])]
     public ?string $password = null;
 
     /**
